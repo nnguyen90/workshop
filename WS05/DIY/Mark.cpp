162d610a7670f861c728d1afb94ed9b52e1ec107
Thy <nnguyen90@myseneca.ca>
2022-12-29 13:01:48 -0500

    workshop

--- WS05/DIY/Mark.cpp
+++ WS05/DIY/Mark.cpp
@@ -5,20 +5,25 @@
 using namespace std;
 namespace sdds
 {
-	Mark::Mark(int mark = 0) {
+	Mark::Mark() {
+		m_mark = 0;
+		isValid = true;
+	};
+
+	Mark::Mark(int mark) {
 		if (0 <= mark && mark <= 100) {
 			this->m_mark = mark;
 			isValid = true;
 		}
 		else {
-			cout << "Invalid";
+			
 			this->m_mark = -1;
 			isValid = false;
 		}
 	};
 
 	Mark::operator int () const {
-		return isValid? m_mark : 0;
+		return isValid? m_mark : -1;
 	};
 
 	Mark::operator double() const {
@@ -44,7 +49,7 @@ namespace sdds
 	};
 
 	Mark::operator char() const {
-		double grade = 0.0;
+		char grade;
 		if (isValid) {
 			if (m_mark < 50) {
 				grade = 'F';
@@ -72,45 +77,209 @@ namespace sdds
 		return (this->isValid);
 	};
 
-	bool Mark::operator == (Mark& m) const {
+	bool Mark::operator == (const Mark& m) const {
 		return this->m_mark == int(m);
 	};
 
-	bool Mark::operator != (Mark& m) const {
+	bool Mark::operator != (const Mark& m) const {
 		return this->m_mark != int(m);
 	};
 
-	bool Mark::operator < (Mark& m) const {
+	bool Mark::operator < (const Mark& m) const {
 		return this->m_mark < int(m);
 	};
 
-	bool Mark::operator > (Mark& m) const {
+	bool Mark::operator > (const Mark& m) const {
 		return this->m_mark > int(m);
 	};
 
-	bool Mark::operator <= (Mark& m) const {
+	bool Mark::operator <= (const Mark& m) const {
 		return this->m_mark <= int(m);
 	};
 
-	bool Mark::operator >= (Mark& m) const {
+	bool Mark::operator >= (const Mark& m) const {
 		return this->m_mark >= int(m);
 	};
 
-	Mark& Mark::operator ++ () const {
+	Mark& Mark::operator ++ (int) {      // var++
+		int oldValue = -1;
 		
+		if (this->isValid) {			
+			oldValue = this->m_mark;
+			this->m_mark++;
+			if (m_mark > 100) {
+				//oldValue = -1;
+				this->isValid = false;
+			}
+		}			
+		Mark res(oldValue);
+		return res;
 	};
 
-	Mark Mark::operator ++ (int mark) const {
-		
+	Mark& Mark::operator ++ () {		  // ++var
+		int newValue = -1;	
+		if (this->isValid) {		
+			this->m_mark++;	
+			newValue = this->m_mark;
+			if (m_mark > 100) {
+				this->isValid = false;
+			}
+		}	
+		Mark res(newValue);
+		return res;
 	};
 
-	Mark& Mark::operator -- () const {
+	Mark& Mark::operator -- (int) {			// var--
+		int oldValue = -1;
+		if (this->isValid) {
+			oldValue = this->m_mark;
+			m_mark--;
+			if (m_mark < 0) {
+				this->isValid = false;
+			}
+		}
+		Mark res(oldValue);
+		return res;
+	};
+
+	Mark& Mark::operator -- () {			 // --var
+		int newValue = -1;
+		if (this->isValid) {
+			this->m_mark--;
+			newValue = this->m_mark;
+			if (m_mark > 100 || m_mark < 0) {
+				this->isValid = false;
+			}
+		}
+		Mark res(newValue);
+		return res;
+	};
+
+	bool Mark::operator ~ () {
+		bool pass = false;
+		if (50 <= this->m_mark && this->m_mark <= 100) {
+			pass = true;
+		}
+		else {
+			pass = false;
+		}
+		return pass;
+	};
+
+	Mark& Mark::operator = (int value) {
+		if (value < 0 || value > 100) {
+			this->m_mark = value;
+			this->isValid = false;
+		}
+		else {
+			this->m_mark = value;
+			this->isValid = true;
+		}
+		return *this;
+	};
+
+	Mark& Mark::operator = (Mark& M) {
+		if (M.isValid) {
+			this->m_mark = M.m_mark;
+			this->isValid = true;		
+		}
+		else {
+			this->m_mark = M.m_mark;
+			this->isValid = false;
+		}
+		return *this;
+	};
+
+	Mark& Mark::operator += (int value) {
+		this->m_mark += value;
+		if (m_mark < 0 || m_mark > 100) {
+			this->m_mark = 0;
+			this->isValid = false;
+		}
+		else {
+			this->isValid = true;
+		}
+		return *this;
+	};
+
+	Mark& Mark::operator -= (int value) {
+		this->m_mark -= value;
+		if (m_mark < 0 || m_mark > 100) {
+			this->m_mark = 0;
+			this->isValid = false;
+		}
+		else {
+			this->isValid = true;
+		}
+		return *this;
 
 	};
 
-	Mark Mark::operator -- (int mark) const {
-
-
+	int operator += (int& value, const Mark& M) {
+		if (M.isValid) {
+			value += M.m_mark;
+		}
+		return value;
 	};
 
+	int operator -= (int& value, const Mark& M) {
+		if (M.isValid) {
+			value -= M.m_mark;
+		}
+		return value;
+	};
+
+	Mark Mark::operator + (int value) const {
+		Mark res = *this;
+		res.m_mark += value;
+		if (res.m_mark < 0 || res.m_mark > 100) {
+			res.m_mark = -1;
+			res.isValid = false;
+		}	
+		else {
+			res.isValid = true;
+		}
+		return res;
+	};
+
+	Mark& Mark::operator + (Mark& M) {
+		if (this->isValid && M.isValid) {
+			this->m_mark += M.m_mark;
+			if (this->m_mark < 0 || this->m_mark > 100) {
+				m_mark = 0;
+				this->isValid = false;
+			}
+		}
+		return *this;
+	};
+
+	Mark& Mark::operator << (Mark& M) {
+		if (this->isValid && M.isValid) {
+			this->m_mark += M.m_mark;
+			M.m_mark = 0;
+			if (this->m_mark < 0 || this->m_mark > 100) {			
+				this->isValid = false;
+			}
+		}
+		return * this;
+	};
+
+	Mark& Mark::operator >> (Mark& M) {
+		if (this->isValid && M.isValid) {
+			M.m_mark += this->m_mark;
+			this->m_mark = 0;
+			if (M.m_mark < 0 || M.m_mark > 100) {
+				M.m_mark = 0;
+				M.isValid = false;
+			}
+		}
+		return *this;
+	};
+
+	int operator + (int& value, const Mark& M) {
+		if (M.isValid) {
+			value += M.m_mark;
+		}
+		return value;
+	};
 }
--- WS05/DIY/Mark.h
+++ WS05/DIY/Mark.h
@@ -8,25 +8,38 @@ namespace sdds
 		int m_mark = 0;
 		bool isValid;
 
+		Mark ();
 		Mark (int mark);
 		operator int() const;
 		operator double() const;
 		operator char() const;
 		operator bool() const;
-		bool operator == (Mark& m) const;
-		bool operator != (Mark& m) const;
-		bool operator < (Mark& m) const;
-		bool operator > (Mark& m) const;
-		bool operator <= (Mark& m) const;
-		bool operator >= (Mark& m) const;
+		bool operator == (const Mark& m) const;
+		bool operator != (const Mark& m) const;
+		bool operator < (const Mark& m) const;
+		bool operator > (const Mark& m) const;
+		bool operator <= (const Mark& m) const;
+		bool operator >= (const Mark& m) const;
 
+		Mark& operator ++ (int) ;
 		Mark& operator ++ () ;
-		Mark operator ++ (int mark) ;
 		
-
-		Mark operator -- (int mark) ;
+		Mark& operator -- (int) ;
 		Mark& operator -- () ;
 
+		bool operator ~ ();
+		Mark& operator = (int);
+		Mark& operator = (Mark& M);
 
+		Mark& operator += (int);
+		Mark& operator -= (int);
+
+		Mark operator + (int value) const ;
+		Mark& operator + (Mark& M);
+		Mark& operator << (Mark& M);
+		Mark& operator >> (Mark& M);
 	};
+	int operator += (int& value, const Mark& M);
+	int operator -= (int& value, const Mark& M);
+	int operator + (int& value, const Mark& M);
 }
