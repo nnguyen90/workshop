fd22e73bb661b9ed1d292365dca045361fb74c1e
Thy <nnguyen90@myseneca.ca>
2023-01-08 07:28:24 -0500

    workshop

--- WS08/lab/Rectangle.cpp
+++ WS08/lab/Rectangle.cpp
@@ -14,15 +14,50 @@ namespace sdds
 		m_height = 0;
 	};
 	Rectangle::Rectangle(const char* label, double width, double height) : LblShape(label) {
-		m_label = nullptr;
-		int len = (int)strlen(label);
-		m_label = new char[len + 1];
-		strcpy(m_label, label);
-	
+		m_width = width;
+		m_height = height;
+		
+		if (m_height < 3 || m_width < strlen(label + 2)) {
+			m_width = 0;
+			m_height = 0;
+		}
+	};
+	void Rectangle::draw(std::ostream& os) {
+		if (m_width != 0 && m_height != 0) {
+			os << '+';
+			os.width(m_width - 2);
+			os.fill('-');
+			os << '+' << endl;
+
+			os << '|';
+			os.width(m_width - 2);
+			os.fill(' ');
+			os.setf(ios::left);
+			os << label();
+			os.unsetf(ios::left);
+			os << '|' << endl;
+
+			for (int i = 0; i < m_height - 4; i++) {
+				os << '|';
+				os.width(m_width - 2);
+				os.fill(' ');
+				os << '|' << endl;
+			}
+			os << '+';
+			os.width(m_width - 2);
+			os.fill('-');
+			os << '+' << endl;
+		}
 	
 	};
-	void Rectangle::draw(std::ostream& os);
-	void Rectangle::getSpecs(std::istream& is);
+
+	void Rectangle::getSpecs(std::istream& is) {
+		LblShape::getSpecs(is);
+		is >> m_width;
+		is.ignore();
+		is >> m_height;
+		is.ignore(1000, '\n');
+	};
 	
 
 
