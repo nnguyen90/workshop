29df5abf4971b2782de4602a9d72b4a2081e99c5
Thy <nnguyen90@myseneca.ca>
2023-01-09 21:23:33 -0500

    workshop

--- WS09/lab/HtmlText.cpp
+++ WS09/lab/HtmlText.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 namespace sdds
 {
-
+	HtmlText::HtmlText() {};
+	HtmlText::HtmlText(HtmlText& H) {};
+	HtmlText& HtmlText::operator = (const HtmlText& H) {};
+	HtmlText::~HtmlText() {};
+	
+	std::ostream& HtmlText::write(std::ostream& os) const {};
 
 
 
--- WS09/lab/Text.cpp
+++ WS09/lab/Text.cpp
@@ -4,8 +4,29 @@
 using namespace std;
 namespace sdds
 {
+	const char& Text::operator[](int index) const {
+		char temp;
+		if (m_content != nullptr || index < strlen(m_content)) {
+			temp = m_content[index];
+		}
+		else {
+			temp = m_content[strlen(m_content)];
+		}
+		return temp;
+	};
 
+	Text::Text() {
+		delete[] m_content;
+		m_content = nullptr;
+	};
+	Text::Text(Text& T) {};
+	Text& Text::operator = (const Text& T) {};
+	Text::~Text() {};
+	std::istream& Text::read(std::istream& is) {};
+	std::ostream& Text::write(std::ostream& os) const {};
+	unsigned Text::getFileLength(istream& is) {};
 
-
-
+	std::istream& operator >> (std::istream& is, Text& T);
+	std::ostream& operator << (std::ostream& os, Text& T);
 }
+
